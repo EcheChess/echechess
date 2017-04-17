dd58ff4587fd8e772a58b836f89a27f2574660c2
Yannick Watier <watier.yannick@gmail.com>
2017-04-17 08:57:11 -0400

    First commit

---
+++ src/main/java/ca/watier/EchechessApplication.java
@@ -0,0 +1,12 @@
+package ca.watier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchechessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchechessApplication.class, args);
+	}
+}
